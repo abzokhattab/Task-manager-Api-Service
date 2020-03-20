996b5c1ad6e61f2c67b260d55cbf2ce8b6b084a8
Abzokhattab <abzokhattab@gmail.com>
2020-03-20 13:05:55 +0100

    mogoose

--- mongodb.js
+++ mongodb.js
@@ -2,10 +2,12 @@
 
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
-
+const ObjectID = mongodb.ObjectID;
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
+const id = new ObjectID();
+console.log(id.getTimestamp());
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!')
@@ -16,5 +18,12 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     db.collection('users').insertOne({
         name: 'Khattab',
         age: 27
-    })
+    },(error,result)=>{
+        if(error){
+            return console.log('unable to insert user')
+        }
+        console.log(result.ops)
+    }
+    
+    )
 })
