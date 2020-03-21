0a0f2dc41bc1a743ee9e18086bd0e0f3ed20bd78
Abzokhattab <abzokhattab@gmail.com>
2020-03-21 11:57:26 +0100

    sorted by feature

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,17 +19,25 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
-    if (req.query.completed){
-        match.completed  = req.query.completed ==='true'
+    const sort = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
     }
-    req.query.completed
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
         await req.user.populate({
-            path:'tasks',
+            path: 'tasks',
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip:parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.send(req.user.tasks)
