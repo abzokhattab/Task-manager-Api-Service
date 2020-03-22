ca951090499cd260e3105860488f3388e3730d04
Abzokhattab <abzokhattab@gmail.com>
2020-03-22 18:44:42 +0100

    cors edit

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const express = require('express')
 require('./db/mangoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-
+const cors = require('cors');
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
